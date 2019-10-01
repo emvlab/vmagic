c6170081b9ea0fb5a488f2a2db1b377e940dc6c6
Diego Vargas <diego.vargas@emvtech.com.br>
2019-10-01 17:26:16 -0300

    .

---
+++ precache-manifest.ab44e512687b1849e0dee5ab8ba301e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8666bae6a7d1b0e0f90250615bed5f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275e69291d237ddb537d",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0a1cc4dbbb66e43c4d53",
+    "url": "/static/css/main.a21e4bb4.chunk.css"
+  },
+  {
+    "revision": "275e69291d237ddb537d",
+    "url": "/static/js/2.0758f3d7.chunk.js"
+  },
+  {
+    "revision": "0a1cc4dbbb66e43c4d53",
+    "url": "/static/js/main.bb83938a.chunk.js"
+  },
+  {
+    "revision": "e3bdc8a85262864ccac0",
+    "url": "/static/js/runtime-main.876fc174.js"
+  },
+  {
+    "revision": "72cfda8ff00c780330e9a34df849883f",
+    "url": "/static/media/logo.72cfda8f.png"
+  }
+]);
